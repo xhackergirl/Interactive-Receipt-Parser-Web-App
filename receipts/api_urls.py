0fe0455fe3d8cb3deda9f80e80209a30968abaa8
xhackergirl <50170324+xhackergirl@users.noreply.github.com>
2025-07-07 14:46:17 +0300

    Add API tests and monthly summary endpoint

--- receipts/api_urls.py
+++ receipts/api_urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('upload/', api_views.upload_receipt_api),
     path('receipts/<int:receipt_id>/edit/', api_views.update_receipt),
+    path('summary/monthly/', api_views.monthly_summary),
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
